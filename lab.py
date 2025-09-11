56e77b484432456a313e3b6549c2e7490d6b6be1
Bsmort <viktory2412@gmail.com>
2025-09-11 22:09:08 +0300

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,16 @@
+def sum_of_two(nums: list, target: int) -> list:
+    step = 0
+    result = []
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == target:
+                result = [i, j]
+                step = 1
+                break
+        if step == 1:
+            break
+    return result 
+print(sum_of_two([2, 7, 11, 15], 9)) 
+print(sum_of_two([3, 2, 4], 6))
+print(sum_of_two([3, 3], 6))
+    
